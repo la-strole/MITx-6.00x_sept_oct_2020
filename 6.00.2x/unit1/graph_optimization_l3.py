181986dbad4d31a2bdb50e7a35c0a611a6e585e7
la-strole <la-strole.sovaleto48@gmail.com>
2020-10-31 16:02:26 +0300

    31 okt

--- 6.00.2x/unit1/graph_optimization_l3.py
+++ 6.00.2x/unit1/graph_optimization_l3.py
@@ -5,6 +5,9 @@ current_node = 1
 n = [current_node]
 count = [1]
 def rec_graph(edges, current_node):
+    """"
+    my code for Kenigsberg bridges problem
+    """
     # 1 base cases
     way_list = [item for item in edges if current_node in item]
 
@@ -37,3 +40,103 @@ def rec_graph(edges, current_node):
     return answer
 
 print(rec_graph(edges, current_node))
+
+
+class Node:
+    def __init__(self, name):
+        self.name = str(name)
+
+    def get_node_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Edge:
+    def __init__(self, src, dest):
+        assert isinstance(src, Node)
+        assert isinstance(dest, Node)
+        self.src = src
+        self.dest = dest
+
+
+    def get_edge(self):
+        return f'{self.src.get_node_name}->{self.dest.get_node_name()}'
+
+    def get_src(self):
+        return self.src
+
+    def get_dest(self):
+        return self.dest
+
+    def __str__(self):
+        return f'{self.src.get_node_name}->{self.dest.get_node_name()}'
+
+
+class Digraph:
+    def __init__(self):
+        self.graph = {} # keys - nodes, values - list of edges
+
+    def add_node(self, node):
+        assert isinstance(node, Node)
+        assert node not in self.graph.keys(), 'node is already in graph'
+        self.graph[node] = []
+
+    def add_edge(self, edge):
+        assert isinstance(edge, Edge)
+        try:
+            self.graph[edge.get_src()].append(edge)
+        except KeyError:
+            self.graph[edge.get_src()] = [edge]
+
+    def children_of(self, node):
+        assert isinstance(node, Node)
+        try:
+            children = []
+            for edges in self.graph[node]:
+                children.append(edges.get_dest())
+            return children
+        except KeyError:
+            print('no such node in graph')
+
+    def __str__(self):
+        for key in self.graph.keys():
+            print(f'{key.get_node_name()}->{[x.get_dest().get_node_name() for x in self.graph[key]]}')
+        return ''
+
+
+class Graph(Digraph):
+
+    def add_edge(self, edge):
+        assert isinstance(edge, Edge)
+        try:
+            self.graph[edge.get_src()].append(edge)
+        except KeyError:
+            self.graph[edge.get_src()] = [edge]
+        tmp_src = edge.get_dest()
+        tmp_dest = edge.get_src()
+        try:
+            self.graph[tmp_src].append(Edge(tmp_src, tmp_dest))
+        except KeyError:
+            self.graph[tmp_src] = [Edge(tmp_src, tmp_dest)]
+
+
+
+g = Graph()
+nodes = [Node(str(x)) for x in range(1, 5)]
+for item in nodes:
+    g.add_node(item)
+
+edges = [Edge(nodes[0], nodes[1]),
+         Edge(nodes[0], nodes[1]),
+         Edge(nodes[1], nodes[2]),
+         Edge(nodes[1], nodes[2]),
+         Edge(nodes[0], nodes[3]),
+         Edge(nodes[2], nodes[3]),
+         Edge(nodes[3], nodes[1])]
+
+for edge in edges:
+    g.add_edge(edge)
+
+print(g)
