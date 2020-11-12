9bad0329ccf42aea33039398c323331af09439f3
la-strole <la-strole.sovaleto48@gmail.com>
2020-11-12 23:19:55 +0300

    12 11 20

--- 6.00.2x/unit2/drunk_man_simulation.py
+++ 6.00.2x/unit2/drunk_man_simulation.py
@@ -1,4 +1,6 @@
 from random import choice
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 class Drunk:
@@ -95,46 +97,130 @@ class Field:
         assert isinstance(start, Location)
         return self.field[d].get_distance(start)
 
+    def get_location(self, d):
+        """
+        return x,y: tuple - current location of d: Drunk
+        """
+        return self.field[d].get_location()
+
+class styleIterator:
+    def __init__(self, styles: list):
+        self.index = 0
+        self.styles = styles
+
+    def next_style(self):
+        result = self.styles[self.index]
+        if self.index == len(self.styles) - 1:
+            self.index = 0
+        else:
+            self.index += 1
+        return result
 
 
-
-def show_distance_normal_drunk(dist, count):
+def show_distance_drunk(dist: list, count: int, type_of_drunk):
+    """
+    dist: list - how many random steps make
+    count: int - how many times repeat each dist trip
+    type_of_drunk - Normal or Cold Drunk
+    return: avg dist from (0.0) for last trip dist[-1]
+    """
     start = Location(0, 0)
 
     for distance in dist:
-        print(f'distance = {distance}')
+        #print(f'distance = {distance}')
         answer = []
         for i in range(count):
             f = Field()
-            man = Normal_Drunk('Homer')
+            man = type_of_drunk('Homer')
             f.add_drunk(man)
             for i in range(distance):
                 f.move_drunk(man)
             answer.append(f.get_distance(man, start))
         answer.sort()
-        print(f'min= {answer[0]},\t max= {answer[-1]},\t avg= {sum(answer)/len(answer)}')
-
-
-def show_distance_cold_drunk(dist, count):
-    start = Location(0, 0)
-
-    for distance in dist:
-        print(f'distance = {distance}')
-        answer = []
-        for i in range(count):
-            f = Field()
-            man = Cold_Drunk('Homer')
-            f.add_drunk(man)
-            for i in range(distance):
-                f.move_drunk(man)
-            answer.append(f.get_distance(man, start))
-        answer.sort()
-        print(f'min= {answer[0]},\t max= {answer[-1]},\t avg= {sum(answer)/len(answer)}')
-
-
+        #print(f'min= {answer[0]},\t max= {answer[-1]},\t avg= {sum(answer) / len(answer)}')
+    return int(sum(answer) / len(answer))
 
 
 trip_dist = [5, 100, 1000, 10000]
-trip_count = 10000
-show_distance_normal_drunk(trip_dist, trip_count)
-show_distance_cold_drunk(trip_dist, trip_count)
+trip_count = 1000
+
+
+# show_distance_drunk(trip_dist, trip_count, Normal_Drunk)
+# show_distance_drunk(trip_dist, trip_count, Cold_Drunk)
+
+
+def x_geomerty_progress(max_number_steps):
+    """
+    returns list of numbers from 1 to max_number
+    """
+    x = 1
+    result = [1]
+    while x < max_number_steps:
+        result.append(int(x * 1.3))
+        x *= 1.3
+    return sorted(list(set(result)))
+
+
+def get_x_y_coordinates(max_number_steps=10000, currancy_count=100, type_of_drunk=Normal_Drunk):
+    x_list = x_geomerty_progress(max_number_steps)
+    y_list = []
+    for x in x_list:
+        y_list.append(show_distance_drunk([x], currancy_count, type_of_drunk))
+        print(f'{int((len(y_list)/len(x_list)) * 100)} percent complete')
+    return x_list, y_list
+
+
+def plot_graph_1():
+    # data for plotting
+    x1, y1 = get_x_y_coordinates()
+
+    fig, ax = plt.subplots()
+    ax.plot(x1, y1, linestyle='solid', label='Normal Drunk walk')
+    ax.set(xlabel='number of steps', ylabel='avg distance from the center', title='Normal/Cold Drunk walk simulation')
+    x2, y2 = get_x_y_coordinates(type_of_drunk=Cold_Drunk)
+    ax.plot(x2, y2, linestyle='dotted', label='Cold Drunk walk')
+    ax.grid()
+    ax.legend()
+    plt.show()
+
+
+def plot_graph_2():
+    # take data
+    number_of_steps = 10000
+    count = 1000
+    x1 = []
+    y1 = []
+    x2 = []
+    y2 = []
+    man1 = Normal_Drunk('Homer')
+    man2 = Cold_Drunk('Nata')
+    for item in range(count):
+        f = Field()
+        f.add_drunk(man1)
+        f.add_drunk(man2)
+        for i in range(number_of_steps):
+            f.move_drunk(man1)
+            f.move_drunk(man2)
+        x1.append(f.get_location(man1)[0])
+        y1.append(f.get_location(man1)[1])
+        x2.append(f.get_location(man2)[0])
+        y2.append(f.get_location(man2)[1])
+        print(f'{int(len(x1)/count*100)} percent complete')
+
+    minimum = min(x1+x2+y1+y2)
+    maximum = max(x1+x2+y1+y2)
+
+
+    fig, ax = plt.subplots()
+    ax.set(xlabel='x', ylabel='y', title=f'Normal/Cold drunk end positions after {number_of_steps} moving')
+    ax.set_xlim(minimum, maximum)
+    ax.set_ylim(minimum, maximum)
+    ax.scatter(x1, y1, c='m', s=2, label='Normal Drunk', marker=r',')
+    ax.scatter(x2, y2, c='c', s=2, label='Cold Drunk')
+    ax.set_box_aspect(1)
+    ax.grid()
+    ax.legend()
+    plt.show()
+
+# plot_graph_1()
+plot_graph_2()
