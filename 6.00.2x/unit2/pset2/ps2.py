a11245501ccb8fe6c541bfdc95352d2f6ffbefc2
la-strole <la-strole.sovaleto48@gmail.com>
2020-11-16 16:15:16 +0300

    16 11 20

--- 6.00.2x/unit2/pset2/ps2.py
+++ 6.00.2x/unit2/pset2/ps2.py
@@ -11,11 +11,13 @@ import pylab
 ##################
 
 # For Python 3.5:
-#from ps2_verify_movement35 import testRobotMovement
+# from ps2_verify_movement35 import testRobotMovement
 # If you get a "Bad magic number" ImportError, you are not using Python 3.5 
 
 # For Python 3.6:
 from ps2_verify_movement36 import testRobotMovement
+
+
 # If you get a "Bad magic number" ImportError, you are not using Python 3.6
 
 
@@ -24,19 +26,20 @@ class Position(object):
     """
     A Position represents a location in a two-dimensional room.
     """
+
     def __init__(self, x, y):
         """
         Initializes a position with coordinates (x, y).
         """
         self.x = x
         self.y = y
-        
+
     def getX(self):
         return self.x
-    
+
     def getY(self):
         return self.y
-    
+
     def getNewPosition(self, angle, speed):
         """
         Computes and returns the new Position after a single clock-tick has
@@ -60,7 +63,7 @@ class Position(object):
         new_y = old_y + delta_y
         return Position(new_x, new_y)
 
-    def __str__(self):  
+    def __str__(self):
         return "(%0.2f, %0.2f)" % (self.x, self.y)
 
 
@@ -73,6 +76,7 @@ class RectangularRoom(object):
     A room has a width and a height and contains (width * height) tiles. At any
     particular time, each of these tiles is either clean or dirty.
     """
+
     def __init__(self, width, height):
         """
         Initializes a rectangular room with the specified width and height.
@@ -87,7 +91,7 @@ class RectangularRoom(object):
         self.width = width
         self.height = height
         self.cleaned = []
-    
+
     def cleanTileAtPosition(self, pos):
         """
         Mark the tile under the position POS as cleaned.
@@ -118,7 +122,7 @@ class RectangularRoom(object):
             return True
         else:
             return False
-    
+
     def getNumTiles(self):
         """
         Return the total number of tiles in the room.
@@ -168,6 +172,7 @@ class Robot(object):
     Subclasses of Robot should provide movement strategies by implementing
     updatePositionAndClean(), which simulates a single time-step.
     """
+
     def __init__(self, room, speed):
         """
         Initializes a Robot with the given speed in the specified room. The
@@ -177,7 +182,13 @@ class Robot(object):
         room:  a RectangularRoom object.
         speed: a float (speed > 0)
         """
-        raise NotImplementedError
+        assert isinstance(room, RectangularRoom)
+        assert type(speed) == float and speed > 0
+        # start position - Position instance
+        self.position = room.getRandomPosition()
+        self.speed = speed
+        # clockwalk from the North
+        self.direction = random.randrange(0, 360)
 
     def getRobotPosition(self):
         """
@@ -185,8 +196,8 @@ class Robot(object):
 
         returns: a Position object giving the robot's position.
         """
-        raise NotImplementedError
-    
+        return self.position
+
     def getRobotDirection(self):
         """
         Return the direction of the robot.
@@ -194,7 +205,7 @@ class Robot(object):
         returns: an integer d giving the direction of the robot as an angle in
         degrees, 0 <= d < 360.
         """
-        raise NotImplementedError
+        return self.direction
 
     def setRobotPosition(self, position):
         """
@@ -202,7 +213,8 @@ class Robot(object):
 
         position: a Position object.
         """
-        raise NotImplementedError
+        assert isinstance(position, Position)
+        self.position = position
 
     def setRobotDirection(self, direction):
         """
@@ -210,7 +222,8 @@ class Robot(object):
 
         direction: integer representing an angle in degrees
         """
-        raise NotImplementedError
+        assert type(direction) == int and 0 <= direction < 360
+        self.direction = direction
 
     def updatePositionAndClean(self):
         """
@@ -219,7 +232,7 @@ class Robot(object):
         Move the robot to a new position and mark the tile it is on as having
         been cleaned.
         """
-        raise NotImplementedError # don't change this!
+        raise NotImplementedError  # don't change this!
 
 
 # === Problem 3
@@ -231,6 +244,7 @@ class StandardRobot(Robot):
     direction; when it would hit a wall, it *instead* chooses a new direction
     randomly.
     """
+
     def updatePositionAndClean(self):
         """
         Simulate the passage of a single time-step.
@@ -266,6 +280,7 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     """
     raise NotImplementedError
 
+
 # Uncomment this line to see how much your simulation takes on average
 ##print(runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot))
 
@@ -276,6 +291,7 @@ class RandomWalkRobot(Robot):
     A RandomWalkRobot is a robot with the "random walk" movement strategy: it
     chooses a new direction at random at the end of each time-step.
     """
+
     def updatePositionAndClean(self):
         """
         Simulate the passage of a single time-step.
@@ -305,7 +321,7 @@ def showPlot1(title, x_label, y_label):
     pylab.ylabel(y_label)
     pylab.show()
 
-    
+
 def showPlot2(title, x_label, y_label):
     """
     What information does the plot produced by this function tell you?
@@ -314,7 +330,7 @@ def showPlot2(title, x_label, y_label):
     times1 = []
     times2 = []
     for width in [10, 20, 25, 50]:
-        height = 300//width
+        height = 300 // width
         print("Plotting cleaning time for a room of width:", width, "by height:", height)
         aspect_ratios.append(float(width) / height)
         times1.append(runSimulation(2, 1.0, width, height, 0.8, 200, StandardRobot))
@@ -326,7 +342,6 @@ def showPlot2(title, x_label, y_label):
     pylab.xlabel(x_label)
     pylab.ylabel(y_label)
     pylab.show()
-    
 
 # === Problem 6
 # NOTE: If you are running the simulation, you will have to close it 
